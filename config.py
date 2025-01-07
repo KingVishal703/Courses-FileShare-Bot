8f4c378a54827dcbc722dec1e8f28d0ee6e93dcf
KingVishal703 <vishal21wankhede@gmail.com>
2025-01-07 20:09:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,82 +1,97 @@
 import re
 import os
 from os import environ
-from Script import script
+from Script import script  # Ensure 'Script' module is available
 
-id_pattern = re.compile(r'^.\d+$')
-def is_enabled(value, default):
-    if value.lower() in ["true", "yes", "1", "enable", "y"]:
-        return True
-    elif value.lower() in ["false", "no", "0", "disable", "n"]:
-        return False
-    else:
-        return default
+# ID validation pattern
+id_pattern = re.compile(r'^-?\d+$')
+
+# Helper function to check boolean values
+def is_enabled(value, default=False):
+    if isinstance(value, str):
+        if value.lower() in ["true", "yes", "1", "enable", "y"]:
+            return True
+        elif value.lower() in ["false", "no", "0", "disable", "n"]:
+            return False
+    return default
 
 # Bot Information
-API_ID = int(environ.get("API_ID", ""))
+API_ID = int(environ.get("API_ID", "0")) if environ.get("API_ID", "0").isdigit() else 0
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_USERNAME = environ.get("BOT_USERNAME", "File_X_Sharing_Bot")  # without @
+PORT = int(environ.get("PORT", "80"))
 
-# force sub cahnel id
+# Validate Bot API
+if not API_ID or not API_HASH or not BOT_TOKEN:
+    raise ValueError("API_ID, API_HASH, and BOT_TOKEN are required!")
+
+# Force subscription channel IDs
 AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002021693647').split()]
 
-PICS = (environ.get('PICS', 'https://graph.org/file/4841dcdcc8b6184847b33-db7006da59fb885b42.jpg https://graph.org/file/6cee69f25da9ae5a833e6-e96e9b204569966133.jpg https://graph.org/file/962a8ff5525ed8640afe8-5a63755db724b4bc37.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5654093580').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "File_X_Sharing_Bot") # without @
-PORT = environ.get("PORT", "80")
+# Bot Start Pictures
+PICS = environ.get('PICS', 
+                   'https://graph.org/file/4841dcdcc8b6184847b33-db7006da59fb885b42.jpg '
+                   'https://graph.org/file/6cee69f25da9ae5a833e6-e96e9b204569966133.jpg '
+                   'https://graph.org/file/962a8ff5525ed8640afe8-5a63755db724b4bc37.jpg').split()
 
-# Clone Info :-
-CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
-# If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
+# Admin IDs
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5654093580').split()]
+
+# Clone Information
+CLONE_MODE = is_enabled(environ.get('CLONE_MODE', "False"))
 CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "")
+
+# Ensure required variables for CLONE_MODE
+if CLONE_MODE and not (CLONE_DB_URI and CDB_NAME):
+    raise ValueError("CLONE_MODE is enabled, but CLONE_DB_URI or CDB_NAME is missing.")
 
 # Database Information
 DB_URI = environ.get("DB_URI", "mongodb+srv://bevag22776:LTYSLtfLKt2KCMnD@cluster0.6z90l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = environ.get("DB_NAME", "Course-Empire")
 
 # Auto Delete Information
-AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
+AUTO_DELETE_MODE = is_enabled(environ.get('AUTO_DELETE_MODE', "True"))
+AUTO_DELETE = int(environ.get("AUTO_DELETE", "20"))  # Time in minutes
+AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1200"))  # Time in seconds
 
-# If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-AUTO_DELETE = int(environ.get("AUTO_DELETE", "20")) # Time in Minutes
-AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1200")) # Time in Seconds
-
-# Channel Information
+# Log Channel
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002309157883"))
 
-# File Caption Information
+# File Caption
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 
-# Enable - True or Disable - False
-PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), False)
+# Public File Store
+PUBLIC_FILE_STORE = is_enabled(environ.get('PUBLIC_FILE_STORE', "False"))
 
-# Verify Info :-
-VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
+# Verify Mode Information
+VERIFY_MODE = is_enabled(environ.get('VERIFY_MODE', "False"))
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "shortxlinks.com")
+SHORTLINK_API = environ.get("SHORTLINK_API", "")
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/hentai_Hanime_Update_Channel/30")
 
-# If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "shortxlinks.com") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "c97dff111e4017c7a0d0f911d567536805cc34c5") # shortlink api
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/hentai_Hanime_Update_Channel/30") # how to open link 
+# Ensure required variables for VERIFY_MODE
+if VERIFY_MODE and not (SHORTLINK_URL and SHORTLINK_API):
+    raise ValueError("VERIFY_MODE is enabled, but SHORTLINK_URL or SHORTLINK_API is missing.")
 
-# Website Info:
-WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
+# Website Information
+WEBSITE_URL_MODE = is_enabled(environ.get('WEBSITE_URL_MODE', "True"))
+WEBSITE_URL = environ.get("WEBSITE_URL", "https://www.kingvshortner.in/2022/07/insurance-coverage-types-inclusions-and.html")
 
-# If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-WEBSITE_URL = environ.get("WEBSITE_URL", "https://www.kingvshortner.in/2022/07/insurance-coverage-types-inclusions-and.html") # For More Information Check Video On Yt - @Tech_VJ
-
-# File Stream Config
-STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
-
-# If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-MULTI_CLIENT = True
+# File Stream Configuration
+STREAM_MODE = is_enabled(environ.get('STREAM_MODE', "True"))
+MULTI_CLIENT = is_enabled(environ.get('MULTI_CLIENT', "True"))
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-if 'DYNO' in environ:
-    ON_HEROKU = True
-else:
-    ON_HEROKU = False
+
+# Heroku Environment Detection
+ON_HEROKU = 'DYNO' in environ
 URL = environ.get("URL", "")
 
-    
+# Final Checks
+if STREAM_MODE and not URL:
+    raise ValueError("STREAM_MODE is enabled, but URL is missing.")
+
+print("Configuration loaded successfully.")
