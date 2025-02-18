c841b8697f94a1a59eb2a4bfa64dac687b54b105
KingVishal703 <vishal21wankhede@gmail.com>
2025-02-18 23:32:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ if VERIFY_MODE and not (SHORTLINK_URL and SHORTLINK_API):
 
 # Website Information
 WEBSITE_URL_MODE = is_enabled(environ.get('WEBSITE_URL_MODE', "True"))
-WEBSITE_URL = environ.get("WEBSITE_URL", "https://technologygyanallinone.blogspot.com/2025/01/samsung-galaxy-s24-ultra-next-level.html")
+WEBSITE_URL = environ.get("WEBSITE_URL", "https://www.moviesking.in.net/2025/01/microsoft-office-rebranding.html")
 
 # File Stream Configuration
 STREAM_MODE = is_enabled(environ.get('STREAM_MODE', "True"))
