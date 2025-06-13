907cf9b733189a5609065cc06b645aa9dafd4dd6
KingVishal703 <vishal21wankhede@gmail.com>
2025-06-14 01:43:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -77,7 +77,7 @@ if VERIFY_MODE and not (SHORTLINK_URL and SHORTLINK_API):
     raise ValueError("VERIFY_MODE is enabled, but SHORTLINK_URL or SHORTLINK_API is missing.")
 
 # Website Information
-WEBSITE_URL_MODE = is_enabled(environ.get('WEBSITE_URL_MODE', "True"))
+WEBSITE_URL_MODE = is_enabled(environ.get('WEBSITE_URL_MODE', "False"))
 WEBSITE_URL = environ.get("WEBSITE_URL", "https://www.aklink.site/2025/01/nothing-phone-3a-ka-honest-review-sab.html")
 
 # File Stream Configuration
