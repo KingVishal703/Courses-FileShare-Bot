b0e7869993f390e8ad268176f8fdb2e8b5f8dba3
KingVishal703 <vishal21wankhede@gmail.com>
2025-10-02 19:25:21 +0530

    Update shortlink.py

--- plugins/shortlink.py
+++ plugins/shortlink.py
@@ -3,9 +3,9 @@ import random
 
 # Example multiple shortlink APIs
 SHORTENER_APIS = [
-    {"base": "https://shortner1.com/api", "key": "apikey1"},
-    {"base": "https://shortner2.com/api", "key": "apikey2"},
-    {"base": "https://shortner3.com/api", "key": "apikey3"},
+    {"base": "https://shortxlinks.com", "key": "c97dff111e4017c7a0d0f911d567536805cc34c5"},
+    {"base": "https://arolinks.com", "key": "07fd488b04eb3d854b8dcc64f7d43c4be189133f"},
+    {"base": "https://dashboard.smallshorts.com", "key": "377565808079977170de7cef039b5db76c49bf42"},
 ]
 
 async def make_shortlink(original_url: str) -> str:
